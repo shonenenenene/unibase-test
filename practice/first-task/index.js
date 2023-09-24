2edeb171a58e4febdb8bf00180919c9459ef84f7
shonenenenene <danya.sytaev@gmail.com>
2023-09-24 20:06:46 +0300

    layout is cooomplete

--- practice/first-task/index.js
+++ practice/first-task/index.js
@@ -1,26 +1,22 @@
-const modalBg = document.querySelector(".contacts"); // Фон попап окна
-const modal = document.querySelector(".modal"); // Само окно
-const openModalButton = document.querySelector(".contacts__btn"); // Кнопки для показа окна
-const closeModalButton = document.querySelector(".modal__cancel-btn"); // Кнопка для скрытия окна
+const modalBg = document.querySelector(".contacts");
+const modal = document.querySelector(".modal"); //
+const openModalButton = document.querySelector(".contacts__btn");
+const closeModalButton = document.querySelector(".modal__cancel-btn");
 
 openModalButton.addEventListener("click", (e) => {
-  // Для каждой вешаем обработчик событий на клик
-  e.preventDefault(); // Предотвращаем дефолтное поведение браузера
-  modalBg.classList.add("active"); // Добавляем класс 'active' для фона
-  modal.classList.add("active"); // И для самого окна
+  e.preventDefault();
+  modalBg.classList.add("active");
+  modal.classList.add("active");
 });
 
 closeModalButton.addEventListener("click", () => {
-  // Вешаем обработчик на крестик
-  modalBg.classList.remove("active"); // Убираем активный класс с фона
-  modal.classList.remove("active"); // И с окна
+  modalBg.classList.remove("active");
+  modal.classList.remove("active");
 });
 
 document.addEventListener("click", (e) => {
-  // Вешаем обработчик на весь документ
   if (e.target === modalBg) {
-    // Если цель клика - фот, то:
-    modalBg.classList.remove("active"); // Убираем активный класс с фона
-    modal.classList.remove("active"); // И с окна
+    modalBg.classList.remove("active");
+    modal.classList.remove("active");
   }
 });
