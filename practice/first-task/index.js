09f0ba92b06d9019bf83d90b31b8f40b7dc52af2
shonenenenene <danya.sytaev@gmail.com>
2023-09-24 22:16:32 +0300

    add func

--- practice/first-task/index.js
+++ practice/first-task/index.js
@@ -3,8 +3,19 @@ const modal = document.querySelector(".modal"); //
 const openModalButton = document.querySelector(".contacts__btn");
 const closeModalButton = document.querySelector(".modal__cancel-btn");
 
-openModalButton.addEventListener("click", (e) => {
-  e.preventDefault();
+const sendFormButton = document.querySelector(".modal__send-btn");
+
+const form = document.querySelector(".modal__form");
+
+// const orgNameInput = document.querySelector(".modal__org-input");
+// const phoneInput = document.querySelector(".modal__phone-input");
+// const mailInput = document.querySelector(".modal__mail-input");
+// const directionInput = document.querySelector(".modal__direction-input");
+
+const reqInputs = document.querySelectorAll(".req-input");
+
+openModalButton.addEventListener("click", (ev) => {
+  ev.preventDefault();
   modalBg.classList.add("active");
   modal.classList.add("active");
 });
@@ -14,9 +25,53 @@ closeModalButton.addEventListener("click", () => {
   modal.classList.remove("active");
 });
 
-document.addEventListener("click", (e) => {
-  if (e.target === modalBg) {
+document.addEventListener("click", (ev) => {
+  if (ev.target === modalBg) {
     modalBg.classList.remove("active");
     modal.classList.remove("active");
   }
+});
+
+const phonePattern = /^\+7 \d{3} \d{3}-\d{2}-\d{2}$/;
+
+[...reqInputs].map((el) => {
+  el.addEventListener("input", () => {
+    console.log(el.name);
+    switch (el.name) {
+      case "org":
+        if (!el.value) {
+          el.classList.add("invalid");
+          sendFormButton.disabled = true;
+        } else {
+          el.classList.remove("invalid");
+          sendFormButton.disabled = false;
+        }
+      case "phone":
+        if (!phonePattern.test(el.value)) {
+          el.classList.add("invalid");
+          sendFormButton.disabled = true;
+        } else {
+          el.classList.remove("invalid");
+          sendFormButton.disabled = false;
+        }
+      case "mail":
+        if (!el.value) {
+          el.classList.add("invalid");
+          sendFormButton.disabled = true;
+        } else {
+          el.classList.remove("invalid");
+          sendFormButton.disabled = false;
+        }
+      case "directions":
+        if (!el.value) {
+          el.classList.add("invalid");
+          sendFormButton.disabled = true;
+        } else {
+          el.classList.remove("invalid");
+          sendFormButton.disabled = false;
+        }
+      default:
+        return;
+    }
+  });
 });
