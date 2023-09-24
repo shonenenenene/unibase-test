2f4e4485743007be6e11e44b95e30016ff654c66
shonenenenene <danya.sytaev@gmail.com>
2023-09-24 22:35:58 +0300

    add one more variant for theory task

--- theory/first-task.js
+++ theory/first-task.js
@@ -9,11 +9,11 @@ const arr = [10, 12, 15, 20];
 // и setTimeout сработает 4 раза с последним значением i, в нашем случае с i = 4.
 // Элемента с таким номером в массиве arr попросту нет.
 
-for (var i = 0; i < arr.length; i++) {
-  setTimeout(function () {
-    console.log(arr[i] > 13 ? `Good: ${arr[i]}` : `Bad: ${arr[i]}`);
-  }, 3000);
-}
+// for (var i = 0; i < arr.length; i++) {
+//   setTimeout(function () {
+//     console.log(arr[i] > 13 ? `Good: ${arr[i]}` : `Bad: ${arr[i]}`);
+//   }, 3000);
+// }
 
 // 1 ВАРИАНТ:
 
@@ -32,6 +32,16 @@ for (let i = 0; i < arr.length; i++) {
 }
 
 // 2 ВАРИАНТ:
+//Можно использовать замыкание.
+for (var i = 0; i < arr.length; i++) {
+  (function (i) {
+    setTimeout(function () {
+      console.log(arr[i] > 13 ? `Good: ${arr[i]}` : `Bad: ${arr[i]}`);
+    }, 3000);
+  })(i);
+}
+
+// 3 ВАРИАНТ:
 
 arr.forEach((e) => {
   setTimeout(function () {
